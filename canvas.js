6cfd7bc496903bc4649210c01763b6becf6bb1fc
haaabla <nhabla@gmail.com>
2017-07-06 20:16:56 +0200

    petition with signature and cookies up and running

--- canvas.js
+++ canvas.js
@@ -3,13 +3,20 @@ const context = canvas.getContext("2d");
 
 $('#canvas').on('mousedown', function(e){
     e.preventDefault();
-    context.moveTo(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+    context.moveTo(e.offsetX, e.offsetY);
     $('#canvas').on('mousemove', function(e){
-        context.lineTo(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+        context.lineTo(e.offsetX, e.offsetY);
         context.stroke();
     }).on('mouseup', function(){
         $('#canvas').off('mousemove');
+        console.log(canvas.toDataURL().length); //base is 3694
         $('#hiddencanvas').val(canvas.toDataURL());
-        console.log(canvas.toDataURL());
     });
 });
+
+$('#clear').on('click', function(e){
+    console.log('cleeeearing');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+$('#button').css('opacity', '0.2');
--- server.js
+++ server.js
@@ -5,26 +5,26 @@ const hb = require('express-handlebars');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const spicedPg = require('spiced-pg');
+const cookieSession = require('cookie-session');
 
 app.use('/public', express.static(__dirname + '/public/'));
-app.use(express.static(__dirname)); //may not need - double check
 
 app.engine('handlebars', hb());
 app.set('view engine', 'handlebars');
 
 //DATABASE
-var db = spicedPg('postgres:postgres:password@localhost:5432/signaturesDB');
+const db = spicedPg('postgres:postgres:password@localhost:5432/signaturesDB');
 
 //COOKIES
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.redirect('/petition')
 });
 
-app.get('/petition', function(req, res) {
-    if (req.cookies.acceptedCookie === 'NomNomNom'){
+app.get('/petition', (req, res) => {
+    if (req.cookies.session === 'eyJpZCI6MX0='){
         res.redirect('/thanks')
     }
     else {
@@ -34,30 +34,41 @@ app.get('/petition', function(req, res) {
     }
 });
 
+//COOKIE MIDDLEWARE
+app.use(cookieSession({
+    secret: 'my own secret code',
+    maxAge: 1000 * 60 * 60 * 24 * 14
+}));
+
 //POST TO DATABASE & SET COOKIE
-app.post('/petition', function(req, res) {
-    db.query("INSERT INTO signatures (firstname, lastname, signature) VALUES ($1,$2,$3)",[req.body.firstname,req.body.lastname,req.body.signature]).then(function(result){
-        console.log(result.rows);
-    }).catch(function(err){
-        console.log(err);
-    });
-    if (res.cookie('acceptedCookie', 'NomNomNom')) {
+app.post('/petition', (req, res) => {
+    db.query("INSERT INTO signatures (firstname, lastname, signature) VALUES ($1,$2,$3) RETURNING id", [req.body.firstname, req.body.lastname, req.body.signature]).then((results) => {
+        req.session.id = results.rows[0].id;
         res.redirect('/thanks');
-    }
+    });
 });
 
 //THANKS
-app.get('/thanks', function(req, res) {
-    res.render('layouts/thanks', {
-        layout: 'main-layout-template',
+app.get('/thanks', (req, res) => {
+    db.query('SELECT signature FROM signatures WHERE id =' + req.session.id).then((results) => {
+        res.render('layouts/thanks', {
+            layout: 'main-layout-template',
+            sigImg: results.rows[0].signature
+        });
     });
 });
 
 //SIGNEES
-app.get('/signees', function(req, res) {
-    res.render('layouts/signees', {
-        layout: 'main-layout-template',
+app.get('/signees', (req, res) => {
+    db.query('SELECT firstname, lastname FROM signatures').then((results) => {
+        res.render('layouts/signees', {
+            layout: 'main-layout-template',
+            listSignees: results.rows
+        });
+    }).catch((err) => {
+        console.log(err);
     });
+
 });
 
 app.listen(8080, () => console.log('-- Port: 8080 Initialised --'));
